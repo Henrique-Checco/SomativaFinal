486019ebcf513d52bf7d5346bdfa736b7d2b8160
Henrique Checco <checcohenrique@gmail.com>
2023-12-13 14:00:22 -0300

    Adicionando Versão Final

---
+++ Somativa/wwwroot/js/JavaScript.js
@@ -0,0 +1,17 @@
+﻿function trocarElementos(cityName, elmnt) {
+    // Hide all elements with class="tabcontent" by default */
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+
+    // Show the specific tab content
+    document.getElementById(cityName).style.display = "block";
+
+
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
